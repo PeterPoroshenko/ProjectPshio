de26b2d566ebcc034db2edfdb945840a7defb78a
Adovos <DragonSpys13@gmail.com>
2018-10-13 18:47:36 +0300

    Добавьте файлы проекта.

---
+++ Intermediate/Build/Win64/ProjectPshioEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "E:/Unreal Projects/ProjectPshio/Intermediate/Build/Win64/ProjectPshioEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/ProjectPshio/ProjectPshioGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PROJECTPSHIO_ProjectPshioGameModeBase_generated_h
+#error "ProjectPshioGameModeBase.generated.h already included, missing '#pragma once' in ProjectPshioGameModeBase.h"
+#endif
+#define PROJECTPSHIO_ProjectPshioGameModeBase_generated_h
+
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_RPC_WRAPPERS
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAProjectPshioGameModeBase(); \
+	friend struct Z_Construct_UClass_AProjectPshioGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AProjectPshioGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/ProjectPshio"), NO_API) \
+	DECLARE_SERIALIZER(AProjectPshioGameModeBase)
+
+
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAProjectPshioGameModeBase(); \
+	friend struct Z_Construct_UClass_AProjectPshioGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AProjectPshioGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/ProjectPshio"), NO_API) \
+	DECLARE_SERIALIZER(AProjectPshioGameModeBase)
+
+
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AProjectPshioGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AProjectPshioGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AProjectPshioGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AProjectPshioGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AProjectPshioGameModeBase(AProjectPshioGameModeBase&&); \
+	NO_API AProjectPshioGameModeBase(const AProjectPshioGameModeBase&); \
+public:
+
+
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AProjectPshioGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AProjectPshioGameModeBase(AProjectPshioGameModeBase&&); \
+	NO_API AProjectPshioGameModeBase(const AProjectPshioGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AProjectPshioGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AProjectPshioGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AProjectPshioGameModeBase)
+
+
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_12_PROLOG
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_RPC_WRAPPERS \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_INCLASS \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ProjectPshio_Source_ProjectPshio_ProjectPshioGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
